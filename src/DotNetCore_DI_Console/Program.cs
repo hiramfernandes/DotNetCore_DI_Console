48efd361ba08b35d7aa0e2f6f5100897ed5bae79
Hiram Fernandes <hiram.fernandes@poatek.com>
2019-09-02 18:28:58 -0300

    Initial commit - working version

---
+++ src/DotNetCore_DI_Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DotNetCore_DI_Console
+{
+    class Program
+    {
+        //Based on the article by Andrew Lock: https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
+
+
+        static void Main(string[] args)
+        {
+            //Setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddScoped<IFileService, FileService>()
+                .BuildServiceProvider()
+                ;
+
+            //Configure Console Logging
+            serviceProvider
+                .GetService<ILoggingBuilder>()
+                //.AddConsole()
+                ;
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+            logger.LogError("Starting Up!");
+
+            //do the actual work here
+            var fileService = serviceProvider.GetService<IFileService>();
+            fileService.WriteToFile("ABC", "test.txt");
+
+            logger.LogDebug("All done!");
+        }
+    }
+
+    interface IFileService
+    {
+        void WriteToFile(string content, string filename);
+    }
+
+    class FileService : IFileService
+    {
+        public void WriteToFile(string content, string filename)
+        {
+
+        }
+    }
+}
---
+++ src/DotNetCore_StructureMapDI_Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNetCore_StructureMapDI_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add the framework services
+
+        }
+    }
+}
